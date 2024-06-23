29f04beaebc044d2c0a941b96861283b044a6644
Michael Sendker <m@standingwater.io>
2024-06-23 15:48:10 -0400

    Remove cruft

--- src/main.c
+++ src/main.c
@@ -1,17 +1,16 @@
 #define LOGGER_H_IMPL
 
-#include "logger.h"
+#include <stdio.h>
 #include "config.h"
 
 int main(int argc, char **argv) {
-    set_logging_level(LOG_LEVEL_DEBUG);
     parse_args(argc, argv);
 
     if (config.outdir != NULL) {
-        log_debug("Output directory '%s'\n", config.outdir);
+        printf("Output directory '%s'\n", config.outdir);
     }
     if (config.host != NULL) {
-        log_debug("Host: '%s'\n", config.host);
+        printf("Host '%s'\n", config.host);
     }
 
     free_config();
